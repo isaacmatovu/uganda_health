ecc114290e175f4de636fec64acde7fc62854367
isaacmatovu <matovuisaac646@gmail.com>
2025-04-29 18:21:57 +0300

    changed backgorund color

--- src/components/common/button.jsx
+++ src/components/common/button.jsx
@@ -1,11 +1,11 @@
-import React,  from "react";
+import React  from "react";
 
 export default function Button(props) {
   const { text,children } = props;
   return (
     <div>
       <button className="bg-[#FFCE00] py-2 px-4 rounded hover:bg-[#FFCE00]/90">
-        <p>{text}</p>{children}
+        <p>{text}</p> {children}
       </button>
     </div>
   );
