30567d76d453da998ac975960b8dd915303edb82
isaacmatovu <matovuisaac646@gmail.com>
2025-04-29 18:21:29 +0300

    added admin page

---
+++ src/app/admin/page.js
@@ -0,0 +1,10 @@
+import { DashboardContent } from "@/components/dashboardContent";
+import { DashboardLayout } from "@/components/dashboardLayout";
+
+export default function Home() {
+  return (
+    <DashboardLayout>
+      <DashboardContent />
+    </DashboardLayout>
+  );
+}
